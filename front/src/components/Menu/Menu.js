633b505451b2c22b8af1a3c76cac529ab8764a82
Kevin sinnadurai <sinnadurai.kevin@hotmail.fr>
2021-09-30 23:03:51 +0200

    Components

--- front/src/components/Menu/Menu.js
+++ front/src/components/Menu/Menu.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Menu.css";
-
+import Input_bar from './Input_bar'
 
  export default function Menu(props) {
   return (
-    <div className="MenuBar">
-      <h1> HELFEN ,{props.name}</h1>
-      <p>Badr</p>
-    </div>
+<div class="topnav" id="myTopnav">
+  <h1>HELFEN</h1>
+  <div class = "input_bar">
+  <Input_bar title="Métier" type="text" max="8" min="4" size="10" id="Profession" name="profession" placeholder="Profession"></Input_bar>
+  <input class = "input_marg" type="text" id="loc  localisation" name="Localisation" required
+  minlength="4" maxlength="8" size="10" placeholder = "Où ?"/>
+  <input class = "input_marg" type="Date" id="date" name="Date" required
+  minlength="4" maxlength="8" size="10" placeholder = "QUand ?"/>
+  <button>Recherche</button>
+  </div>
+
+</div>
   );
 }
 
