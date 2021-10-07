287512c025c352d5663155780a5bea28e8f6207d
Kevin sinnadurai <sinnadurai.kevin@hotmail.fr>
2021-10-07 14:26:30 +0200

    front route page

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/Menu/Menu.js'
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
 
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+);
+
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Menu name = "test"></Menu>
-      </header>
+    <div>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/category">Category</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+
+      { /* Route components are rendered if the path prop matches the current URL */}
+      <Route path="/"><Home /></Route>
+      <Route path="/category"><Category /></Route>
+      <Route path="/products"><Products /></Route>
     </div>
   );
 }
