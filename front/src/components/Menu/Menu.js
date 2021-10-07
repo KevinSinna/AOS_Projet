d665e806fc31cf5ccb81e64e2127455c176c92ec
Kevin sinnadurai <sinnadurai.kevin@hotmail.fr>
2021-10-07 13:13:15 +0200

    front signup

--- front/src/components/Menu/Menu.js
+++ front/src/components/Menu/Menu.js
@@ -7,7 +7,7 @@ import Input_bar from './Input_bar'
 <div class="topnav" id="myTopnav">
   <h1>HELFEN</h1>
   <div class = "input_bar">
-  <Input_bar title="Métier" type="text" max="8" min="4" size="10" id="Profession" name="profession" placeholder="Profession"></Input_bar>
+  <Input_bar title="Métier" type="text" max="8" min="4" size="10" id="Profession" name="profession" placeholder="Profession"/>
   <input class = "input_marg" type="text" id="loc  localisation" name="Localisation" required
   minlength="4" maxlength="8" size="10" placeholder = "Où ?"/>
   <input class = "input_marg" type="Date" id="date" name="Date" required
