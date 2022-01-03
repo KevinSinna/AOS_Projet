39d2332c92aa5f8de04ae99a5f4dddde2564b4c3
BadrHamd <83960884+BadrHamd@users.noreply.github.com>
2022-01-03 17:04:37 +0100

    V
    ss

--- front/src/App.js
+++ front/src/App.js
@@ -2,50 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Menu from './components/Menu/Menu.js'
 import React from "react";
-import { Link, Route, Switch } from "react-router-dom";
-import FilterTableService from './components/Service/FilterTableService';
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-
-const Category = () => (
-  <div>
-    <h2>Category</h2>
-  </div>
-);
-
-const Products = () => (
-  <div>
-    <h2>Products</h2>
-  </div>
-);
+import {BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './page/Home'
+import Result from './page/Result'
+import Connexion from './page/Connexion'
+import Inscription from './page/Inscription'
+import ProfilClient from './page/ProfilClient';
 function App() {
   return (
-    <FilterTableService avatar="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" nom = "Kev" prenom = "N" profession = "bg" localisation ="Evry"></FilterTableService>
-  );
-}
-
+  <BrowserRouter>
+    <Switch>
+      <Route path= "/" exact component={Home}/>
+      <Route path="/result" exact component={Result}/>
+      <Route path= "/connexion" exact component={Connexion}/>
+      <Route path= "/inscription" exact component={Inscription}/>
+      <Route path= "/moncompte" exact component={ProfilClient}/>
+    </Switch>
+  </BrowserRouter>
+  )}
 export default App;
-/*<div>
-      <nav className="navbar navbar-light">
-        <ul className="nav navbar-nav">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/category">Category</Link>
-          </li>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-        </ul>
-      </nav>
-
-      { /* Route components are rendered if the path prop matches the current URL */
-     /* <Route path="/"><Home /></Route>
-      <Route path="/category"><Category /></Route>
-      <Route path="/products"><Products /></Route>
-    </div>*/
