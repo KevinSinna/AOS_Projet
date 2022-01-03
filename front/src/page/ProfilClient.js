9812187e746a5ed7c83a1e3f43be9d1114e90dbb
BadrHamd <83960884+BadrHamd@users.noreply.github.com>
2022-01-03 17:45:22 +0100

    commit profil

--- front/src/page/ProfilClient.js
+++ front/src/page/ProfilClient.js
@@ -1,5 +1,5 @@
   import React from 'react';
-  import Navbar from '../components/NavBar/Navbar';
+  import Navbar from '../components/NavBar/Navbar2';
   import InfoClient from '../components/Profil/InfoClient';
   import Prestationprofil from '../components/Profil/Prestationencours';
 
@@ -8,7 +8,7 @@
           <div>
               <Navbar/>
               <InfoClient/>
-              <Prestationprofil/>
+              
           </div>
       );
   };
