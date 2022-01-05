c6c28c965f370d45df64c29de9887463480f1ca3
Fluox0 <59915251+Fluox0@users.noreply.github.com>
2022-01-05 20:47:42 +0100

    reset

--- front/src/page/Result.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import FilterTableService from '../components/Service/FilterTableService';
-
-function App() {
-  return (
-    <FilterTableService avatar="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" nom = "Kev" prenom = "N" profession = "bg" localisation ="Evry"></FilterTableService>
-  );}
-  export default App;
