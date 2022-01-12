2116e72a1273038e985f878396e197902d1d742e
Kevin sinnadurai <sinnadurai.kevin@hotmail.fr>
2022-01-12 16:50:26 +0100

    update date

--- front/src/components/List/Card_reservation.js
+++ front/src/components/List/Card_reservation.js
@@ -34,7 +34,21 @@ export default class Card_reservation extends React.Component{
                 <label htmlFor="Nom" className="sr-only">
                   Date de début
                 </label>
-                <select name="pets" id="pet-select">
+                <select name="date" id="date-select" class="form-select appearance-none
+      block
+      w-full
+      px-3
+      py-1.5
+      text-base
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding bg-no-repeat
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
     <option >--Please choose an option--</option>
     <option >27/01/2021</option>
     <option >28/01/2021</option>
